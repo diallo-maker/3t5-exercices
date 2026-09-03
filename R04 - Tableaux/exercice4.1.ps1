ede5deb034cea1d2f400656fe176b3bd5f0823cc
diallo-maker <1856207@cegepmontpetit.ca>
2026-09-03 14:58:23 -0400

    fin l'excercice

--- R04 - Tableaux/exercice4.1.ps1
+++ R04 - Tableaux/exercice4.1.ps1
@@ -2,22 +2,24 @@
 
 # 4.1.1. Obtenir tous les fichiers contenus dans le répertoire C:\Windows.
    
-
+get-childItem -path C:\Windows\ -File  
 
 # 4.1.2. Obtenir tous les fichiers contenus dans le répertoire C:\Windows, triés par ordre 
 #        décroissant de taille.
 
+get-childItem -path C:\Windows\ -File | sort-object -Property Length -Descending
 
 
 # 4.1.3. Même chose, mais montrer seulement les fichiers plus grands que 1 mégaoctet.
 
+get-childItem -path C:\Windows\ -File | Where-Object -Property Length -gt 1mb | sort-object -Property Length -Descending
 
+# 4.1.4. Même chose, mais montrer seulement les fichiers qui pèsent entre 1 et 100 kilooctets.
 
-# 4.1.4. Même chose, mais montrer seulement les fichiers qui pèsent entre 1 et 10 mégaoctets.
-
+get-childItem -path C:\Windows\ -File | Where-Object {$_.Length -ge 100kb -and $_.Length -le 1mb  }| sort-object -Property Length -Descending 
 
 
 # 4.1.5. Même chose, mais montrer seulement les fichiers qui pèsent soit plus de 1 mégaoctet 
 #        ou moins de 1 kilooctet.
 
-
+get-childItem -path C:\Windows\ -File | Where-Object {$_.Length -gt 1mb -or $_.Length -lt 1kb }| sort-object -Property Length -Descending 
--- R04 - Tableaux/exercice4.2.ps1
+++ R04 - Tableaux/exercice4.2.ps1
@@ -3,14 +3,18 @@
 # 4.2.1. Obtenir la liste de tous les services dont le nom commence par W et afficher les 
 #        propriétés Name, Status et StartType dans un tableau.
 
-
+get-service | where-object -Property Name -like "W*" | Select-Object Name, Status, StartType  | Format-Table
 
 # 4.2.2. Même chose, mais montrer seulement les services en cours d'exécution.
 
-
+get-service | where-object {$_.Name -like "W*" -and $_.Status -eq "Running" } | Select-Object Name, Status, StartType  | Format-Table
 
 # 4.2.3. Même chose, mais montrer seulement les services en cours d'exécution qui 
 #        s'exécutent automatiquement.
 
+Get-Service | 
+>> Where-Object {$_.Name -like "W*" -and $_.StartType -eq "Automatic"} |
+>> ForEach-Object {
+>>     Get-Service -Name $_.Name | Format-List * | Out-File "Service_$($_.Name).txt"}
 
-
+# dans le ForEach-Object, le get-service -Name $_.name prend le nom du service qui est le courant et prend le service donc tout
