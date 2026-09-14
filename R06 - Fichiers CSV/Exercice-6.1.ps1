f920d8c7e3b1785041bf19595235bf58292b402a
diallo-maker <1856207@cegepmontpetit.ca>
2026-09-14 13:42:09 -0400

    Fin exercise R06

--- R06 - Fichiers CSV/Exercice-6.1.ps1
+++ R06 - Fichiers CSV/Exercice-6.1.ps1
@@ -6,16 +6,16 @@
 
 # 1. Dans PowerShell, utilisez la commande Get-Content pour voir le contenu de ce fichier texte.
 
-
+get-content -Path "C:\Users\1856207\Downloads\stawars.csv"
 
 # 2. Utilisez maintenant la commande Import-Csv pour importer le fichier sous forme d'un tableau d'objets.
 
-
+$starwars = Import-Csv -Path "C:\Users\1856207\Downloads\stawars.csv" -Delimiter ';'
 
 
 # 3. Donnez le prénom et le nom des contrebandiers, en ordre alphabétique de nom.
 
-
+$contrebandiers = $starwars | Where-object -Property Profession -Match "Contrebandier" | select-object -Property Prenom, Nom | sort-object -Property Nom 
 
 
 # 4. Sauvegardez ces nouvelles données dans le fichier contrebandiers.csv afin qu'il puisse être ouvert dans Excel
@@ -23,5 +23,5 @@
 #    vous utilisez Windows PowerShell). Le fichier devrait pouvoir être ouvert dans Excel sans erreur.
 
 
-
+$contrebandiers | Export-Csv -Path "C:\Users\1856207\Downloads\contrbandiers.csv" -Delimiter ';'
 
